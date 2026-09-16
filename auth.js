57486901f76f1e411c14a542d6631bdb978df59b
minhthuan-suty <nguyenminhthuan1107@gmail.com>
2026-09-16 19:20:40 +0700

    Change admin password from '9999' to '123'

--- auth.js
+++ auth.js
@@ -21,7 +21,7 @@ function login(username, password) {
     }
 
     // Kiểm tra tài khoản và mật khẩu
-    if (username === "admin" && password === "9999") {
+    if (username === "admin" && password === "123") {
         return true;
     }
 
